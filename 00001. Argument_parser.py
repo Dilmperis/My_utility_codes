a13f3ed8b6b88eb1b21bf1b0c73e569a2f7ed16f
Giannis Dilmperis <dil.john2@hotmail.com>
2025-07-14 18:28:14 +0200

    Update 00001. Argument_parser.py

--- 00001. Argument_parser.py
+++ 00001. Argument_parser.py
@@ -14,3 +14,20 @@ def parse_arguments():
                         help='The path to the dataset directory')
     
     return parser.parse_args()
+
+
+##########################################################################################
+# Or more effeciently:
+'''14/7/2025'''
+
+parser = argparse.ArgumentParser(description="Train a model")
+
+parser.add_argument('--batch_size', type=int, default=16, help='Batch size')
+parser.add_argument('--lr', type=float, default=0.001, help='Learning rate')
+parser.add_argument('--use_gpu', action='store_true', help='Use GPU if available')
+
+args = parser.parse_args()
+
+print(f"Batch size: {args.batch_size}")
+print(f"Learning rate: {args.lr}")
+print(f"Use GPU: {args.use_gpu}")
