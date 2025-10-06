5fe1541390188677e8c05fbe22fc4ea64dc9d6a5
Giannis Dilmperis <dil.john2@hotmail.com>
2025-10-06 13:55:31 +0200

    Update 00003. Open_various_files.py

--- 00003. Open_various_files.py
+++ 00003. Open_various_files.py
@@ -31,6 +31,28 @@
 #     data = f.get('dataset_name') # THis works the same ways as data = f['dataset_name'] 
 #     print("Shape of the dataset:", data.shape)
 
+# EXTRA CODE FROM OTHERS:
+# with h5py.File(filename, "r") as f:
+#     # Print all root level object names (aka keys) 
+#     # these can be group or dataset names 
+#     print("Keys: %s" % f.keys())
+#     # get first object name/key; may or may NOT be a group
+#     a_group_key = list(f.keys())[0]
+
+#     # get the object type for a_group_key: usually group or dataset
+#     print(type(f[a_group_key])) 
+
+#     # If a_group_key is a group name, 
+#     # this gets the object names in the group and returns as a list
+#     data = list(f[a_group_key])
+
+#     # If a_group_key is a dataset name, 
+#     # this gets the dataset values and returns as a list
+#     data = list(f[a_group_key])
+#     # preferred methods to get dataset values:
+#     ds_obj = f[a_group_key]      # returns as a h5py dataset object
+#     ds_arr = f[a_group_key][()]  # returns as a numpy array
+
 # .npz ################################################################################################
 # import numpy as np
 # file_path = #add npz file here
@@ -56,5 +78,6 @@
 #         # print(member.name)
 #         i+=1
 #     print(f'Number of folders: {i}')
+
 
 
