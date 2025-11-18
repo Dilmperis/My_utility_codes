0831e56934897d7e464de3b166660f16096253f9
Giannis Dilmperis <dil.john2@hotmail.com>
2025-11-18 23:28:02 +0100

    Create 00026. Upload images in a script.py

---
+++ 00026. Upload images in a script.py
@@ -0,0 +1,19 @@
+# 3 Methods to upload images in a script from a folder:
+
+# Pillow (PIL)
+import os
+from PIL import Image
+import  numpy as np
+
+path_folder = "path/to/folder"
+dataset = []
+
+for file in os.listfolder(path_folder):
+  if file.endswith('.png'):
+    image_path = os.path.join(path_folder, file)
+    img = Image.open(image_path)
+    img_as_array = np.array(img)
+    dataset.append(img_as_array)
+
+dataset = np.array(dataset)
+print(dataset.shape)
